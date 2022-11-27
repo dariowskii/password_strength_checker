2d8b4e95ab9c71128fe953abca89087fbf5924bc
Dario Varriale <dario.varriale@atobit.it>
2022-11-27 19:22:36 +0100

    Updated exports

--- lib/password_strength_checker.dart
+++ lib/password_strength_checker.dart
@@ -4,10 +4,15 @@ library password_strength_checker;
 // UI
 export 'src/password_strength_checker.dart';
 export 'src/password_strength_form_checker.dart';
+// Utils
+export 'src/password_generator/password_generator.dart';
 // Configurations
 export 'src/config/password_strength_checker_configuration.dart';
 export 'src/config/text_form_field_configuration.dart';
+export 'src/config/password_generator_configuration.dart';
 // Models
 export 'src/dict/common_passwords.dart';
-// Utils
+// Enums
 export 'src/enum/password_strength_enum.dart';
+// Extensions
+export 'src/extensions/text_form_field_extension.dart';
