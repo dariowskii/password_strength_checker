a6d9a1cadc27bfc5c38561e07bfd49716efe12e1
Dario Varriale <dario.varriale@atobit.it>
2022-11-27 19:23:19 +0100

    Implemented password generator support

--- lib/src/password_strength_form_checker.dart
+++ lib/src/password_strength_form_checker.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import '../password_strength_checker.dart';
 
 /// The widget to use into a [Form] widget.
@@ -42,6 +43,14 @@ class PasswordStrengthFormChecker<T extends PasswordStrengthItem>
     ),
     this.passwordStrengthCheckerConfiguration =
         const PasswordStrengthCheckerConfiguration(),
+    this.showGenerator = false,
+    this.passwordGeneratorConfiguration =
+        const PasswordGeneratorConfiguration(),
+    this.onPasswordGenerated,
+    this.generateButtonStyle,
+    this.generateButtonChild = const Text("Generate Password"),
+    this.generateButtonSize = const Size(double.maxFinite, 48),
+    this.automaticallyFillWithGeneratedPassword = true,
   }) : super(key: key);
 
   /// The function that will be called when the text changes. You need to update the [ValueNotifier] to update the widget.
@@ -81,6 +90,28 @@ class PasswordStrengthFormChecker<T extends PasswordStrengthItem>
   final PasswordStrengthCheckerConfiguration
       passwordStrengthCheckerConfiguration;
 
+  /// If true, a button to generate a password will be shown.
+  final bool showGenerator;
+
+  /// The configuration for the [PasswordGenerator].
+  final PasswordGeneratorConfiguration passwordGeneratorConfiguration;
+
+  /// The function that will be called when the password is generated.
+  final Function(String password, ValueNotifier<T?> strengthNotifier)?
+      onPasswordGenerated;
+
+  /// The style for the generate button.
+  final ButtonStyle? generateButtonStyle;
+
+  /// The child for the generate button.
+  final Widget? generateButtonChild;
+
+  /// The size for the generate button.
+  final Size generateButtonSize;
+
+  /// Fill the fields with the generated [password].
+  final bool automaticallyFillWithGeneratedPassword;
+
   @override
   State<PasswordStrengthFormChecker<T>> createState() =>
       _PasswordStrengthFormCheckerState<T>();
@@ -92,139 +123,47 @@ class _PasswordStrengthFormCheckerState<T extends PasswordStrengthItem>
 
   @override
   Widget build(BuildContext context) {
+    final passwordGenerator = PasswordGenerator.fromConfig(
+      configuration: widget.passwordGeneratorConfiguration,
+    );
+    
     return Column(
       children: [
         if (widget.topInstructions != null) widget.topInstructions!,
-        TextFormField(
-          decoration: widget.textFormFieldConfiguration.decoration,
-          onChanged: (value) => widget.onChanged(value, passNotifier),
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return widget.hideErrorMessage
-                  ? ''
-                  : widget.emptyTextErrorMessage;
-            }
-            final strength = passNotifier.value!;
-            if (strength.index < widget.minimumStrengthRequired.index) {
-              return widget.hideErrorMessage ? '' : widget.errorMessage;
-            }
-            return null;
-          },
-          key: widget.textFormFieldConfiguration.key,
-          controller: widget.textFormFieldConfiguration.controller,
-          focusNode: widget.textFormFieldConfiguration.focusNode,
-          autofocus: widget.textFormFieldConfiguration.autofocus,
-          textInputAction: widget.textFormFieldConfiguration.textInputAction,
-          textCapitalization:
-              widget.textFormFieldConfiguration.textCapitalization,
-          style: widget.textFormFieldConfiguration.style,
-          textAlign: widget.textFormFieldConfiguration.textAlign,
-          textDirection: widget.textFormFieldConfiguration.textDirection,
-          readOnly: widget.textFormFieldConfiguration.readOnly,
-          toolbarOptions: widget.textFormFieldConfiguration.toolbarOptions,
-          showCursor: widget.textFormFieldConfiguration.showCursor,
-          obscuringCharacter:
-              widget.textFormFieldConfiguration.obscuringCharacter,
-          obscureText: widget.textFormFieldConfiguration.obscureText,
-          autocorrect: widget.textFormFieldConfiguration.autocorrect,
-          smartDashesType: widget.textFormFieldConfiguration.smartDashesType,
-          smartQuotesType: widget.textFormFieldConfiguration.smartQuotesType,
-          enableSuggestions:
-              widget.textFormFieldConfiguration.enableSuggestions,
-          maxLines: widget.textFormFieldConfiguration.maxLines,
-          minLines: widget.textFormFieldConfiguration.minLines,
-          expands: widget.textFormFieldConfiguration.expands,
-          maxLength: widget.textFormFieldConfiguration.maxLength,
-          maxLengthEnforcement:
-              widget.textFormFieldConfiguration.maxLengthEnforcement,
-          onEditingComplete:
-              widget.textFormFieldConfiguration.onEditingComplete,
-          onFieldSubmitted: widget.textFormFieldConfiguration.onFieldSubmitted,
-          onSaved: widget.textFormFieldConfiguration.onSaved,
-          inputFormatters: widget.textFormFieldConfiguration.inputFormatters,
-          enabled: widget.textFormFieldConfiguration.enabled,
-          cursorWidth: widget.textFormFieldConfiguration.cursorWidth,
-          cursorHeight: widget.textFormFieldConfiguration.cursorHeight,
-          cursorRadius: widget.textFormFieldConfiguration.cursorRadius,
-          cursorColor: widget.textFormFieldConfiguration.cursorColor,
-          keyboardAppearance:
-              widget.textFormFieldConfiguration.keyboardAppearance,
-          scrollPadding: widget.textFormFieldConfiguration.scrollPadding,
-          enableInteractiveSelection:
-              widget.textFormFieldConfiguration.enableInteractiveSelection,
-          selectionControls:
-              widget.textFormFieldConfiguration.selectionControls,
-          onTap: widget.textFormFieldConfiguration.onTap,
-          buildCounter: widget.textFormFieldConfiguration.buildCounter,
-          scrollPhysics: widget.textFormFieldConfiguration.scrollPhysics,
-          autofillHints: widget.textFormFieldConfiguration.autofillHints,
-          autovalidateMode: widget.textFormFieldConfiguration.autovalidateMode,
-          restorationId: widget.textFormFieldConfiguration.restorationId,
-          enableIMEPersonalizedLearning:
-              widget.textFormFieldConfiguration.enableIMEPersonalizedLearning,
-          mouseCursor: widget.textFormFieldConfiguration.mouseCursor,
+        TextFormField().fromConfig(
+          configuration: widget.textFormFieldConfiguration.copyWith(
+            onChanged: (value) => widget.onChanged(value, passNotifier),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return widget.hideErrorMessage
+                    ? ''
+                    : widget.emptyTextErrorMessage;
+              }
+              final strength = passNotifier.value!;
+              if (strength.index < widget.minimumStrengthRequired.index) {
+                return widget.hideErrorMessage ? '' : widget.errorMessage;
+              }
+              return null;
+            },
+          ),
         ),
         if (widget.needConfirmation &&
             widget.textFormFieldConfiguration.controller != null) ...[
           const SizedBox(height: 10),
-          TextFormField(
-            decoration:
-                widget.confirmationTextFormFieldConfiguration.decoration,
-            validator: (value) {
-              if (value == null ||
-                  (value.isEmpty &&
-                      !widget.hideConfirmationErrorMessageWhenEmpty) ||
-                  value != widget.textFormFieldConfiguration.controller!.text) {
-                return widget.confirmationErrorMessage;
-              }
-              return null;
-            },
-            key: widget.confirmationTextFormFieldConfiguration.key,
-            controller:
-                widget.confirmationTextFormFieldConfiguration.controller,
-            focusNode: widget.confirmationTextFormFieldConfiguration.focusNode,
-            autofocus: widget.confirmationTextFormFieldConfiguration.autofocus,
-            textInputAction:
-                widget.confirmationTextFormFieldConfiguration.textInputAction,
-            textCapitalization: widget
-                .confirmationTextFormFieldConfiguration.textCapitalization,
-            style: widget.confirmationTextFormFieldConfiguration.style,
-            textAlign: widget.confirmationTextFormFieldConfiguration.textAlign,
-            textDirection:
-                widget.confirmationTextFormFieldConfiguration.textDirection,
-            readOnly: widget.confirmationTextFormFieldConfiguration.readOnly,
-            toolbarOptions:
-                widget.confirmationTextFormFieldConfiguration.toolbarOptions,
-            showCursor:
-                widget.confirmationTextFormFieldConfiguration.showCursor,
-            obscuringCharacter: widget
-                .confirmationTextFormFieldConfiguration.obscuringCharacter,
-            obscureText:
-                widget.confirmationTextFormFieldConfiguration.obscureText,
-            autocorrect:
-                widget.confirmationTextFormFieldConfiguration.autocorrect,
-            smartDashesType:
-                widget.confirmationTextFormFieldConfiguration.smartDashesType,
-            smartQuotesType:
-                widget.confirmationTextFormFieldConfiguration.smartQuotesType,
-            enableSuggestions:
-                widget.confirmationTextFormFieldConfiguration.enableSuggestions,
-            maxLines: widget.confirmationTextFormFieldConfiguration.maxLines,
-            minLines: widget.confirmationTextFormFieldConfiguration.minLines,
-            expands: widget.confirmationTextFormFieldConfiguration.expands,
-            maxLength: widget.confirmationTextFormFieldConfiguration.maxLength,
-            maxLengthEnforcement: widget
-                .confirmationTextFormFieldConfiguration.maxLengthEnforcement,
-            onEditingComplete:
-                widget.confirmationTextFormFieldConfiguration.onEditingComplete,
-            onFieldSubmitted:
-                widget.confirmationTextFormFieldConfiguration.onFieldSubmitted,
-            onSaved: widget.confirmationTextFormFieldConfiguration.onSaved,
-            inputFormatters:
-                widget.confirmationTextFormFieldConfiguration.inputFormatters,
-            enabled: widget.confirmationTextFormFieldConfiguration.enabled,
-            mouseCursor:
-                widget.confirmationTextFormFieldConfiguration.mouseCursor,
+          TextFormField().fromConfig(
+            configuration:
+                widget.confirmationTextFormFieldConfiguration.copyWith(
+              validator: (value) {
+                if (value == null ||
+                    (value.isEmpty &&
+                        !widget.hideConfirmationErrorMessageWhenEmpty) ||
+                    value !=
+                        widget.textFormFieldConfiguration.controller!.text) {
+                  return widget.confirmationErrorMessage;
+                }
+                return null;
+              },
+            ),
           ),
         ],
         const SizedBox(height: 20),
@@ -232,6 +171,34 @@ class _PasswordStrengthFormCheckerState<T extends PasswordStrengthItem>
           strength: passNotifier,
           configuration: widget.passwordStrengthCheckerConfiguration,
         ),
+        if (widget.showGenerator) ...[
+          const SizedBox(height: 20),
+          SizedBox(
+            width: widget.generateButtonSize.width,
+            height: widget.generateButtonSize.height,
+            child: ElevatedButton(
+              style: widget.generateButtonStyle ??
+                  ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black87,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+              onPressed: () {
+                final password = passwordGenerator.generate();
+
+                if (widget.automaticallyFillWithGeneratedPassword) {
+                  widget.textFormFieldConfiguration.controller?.text = password;
+                  widget.confirmationTextFormFieldConfiguration.controller
+                      ?.text = password;
+                }
+
+                widget.onPasswordGenerated?.call(password, passNotifier);
+              },
+              child: widget.generateButtonChild,
+            ),
+          ),
+        ],
       ],
     );
   }
